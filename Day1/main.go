5a7409c976eb555e4b4dfed19b64d19867b3bfc2
= <BrandonGong@users.noreply.github.com>
2023-12-01 20:08:06 -0700

    Day1 part 2

--- Day1/main.go
+++ Day1/main.go
@@ -6,23 +6,50 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
+
+var searchStrings []string
 
 func main() {
 	input, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
-	re := regexp.MustCompile(`\d`)
+
+	searchStrings = []string{"\\d", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	re := regexp.MustCompile(strings.Join(searchStrings, "|"))
 	fs := bufio.NewScanner(input)
 	sum := 0
 	for fs.Scan() {
 		line := fs.Text()
+		// Handle edge cases where two digits are merged together
+		line = strings.ReplaceAll(line, "eightwo", "82")
+		line = strings.ReplaceAll(line, "twone", "21")
+		line = strings.ReplaceAll(line, "oneight", "18")
+		// Find all matches of search strings in line
 		numbers := re.FindAllString(line, -1)
-		calibrationValue, _ := strconv.Atoi(numbers[0] + numbers[len(numbers)-1])
+		first := ParseNumber(numbers[0])
+		last := ParseNumber(numbers[len(numbers)-1])
+		// Make calibration value as two digit number from first and last digits found
+		calibrationValue, _ := strconv.Atoi(fmt.Sprintf("%d%d", first, last))
 
-		fmt.Printf("In Line: %s, Found numbers: %v, Calibration Number: %d\n", line, numbers, calibrationValue)
+		//fmt.Printf("In Line: %s, Found numbers: %v, Calibration Number: %d\n", line, numbers, calibrationValue)
+		//fmt.Println(calibrationValue)
 		sum += calibrationValue
 	}
 	fmt.Printf("Total Value: %d\n", sum)
+}
+
+func ParseNumber(value string) int {
+	num, err := strconv.Atoi(value)
+	if err == nil {
+		return num
+	}
+	for i, s := range searchStrings {
+		if s == value {
+			return i
+		}
+	}
+	panic("Value not found: " + value)
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AOC2023
 Advent of Code 2023
+
+# Day 1: trebuchet?
+Given a calibration document, find the sum of all calibration numbers.
+
+**Part 1:**
+The calibration number is given as the first and last digit of the line. For example, `1abc2` would be 12.
+
+**Part 2:**
+The calibration numbers can also include digits written out, for example one, two, three, and so on. With this new information, find the new sum of the calibration numbers.
