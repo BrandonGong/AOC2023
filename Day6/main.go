9b4b15124171997c681d46c31d5c5ce3b00eb944
= <BrandonGong@users.noreply.github.com>
2023-12-09 11:26:06 -0700

    Day6 pt2

--- Day6/main.go
+++ Day6/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	shared "aoc2023/Shared"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	lines := shared.ReadInputFile("input.txt")
+	Part1(lines)
+	Part2(lines)
+}
+func Part1(lines []string) {
 	times := shared.ParseIntsFromLine(lines[0])
 	recordDistances := shared.ParseIntsFromLine(lines[1])
 	part1Product := 1
@@ -22,4 +28,16 @@ func main() {
 		part1Product *= winOptions
 	}
 	fmt.Printf("Part1: %d\n", part1Product)
+}
+func Part2(lines []string) {
+	time, _ := strconv.Atoi(strings.ReplaceAll(strings.Split(lines[0], ":")[1], " ", ""))
+	recordDistance, _ := strconv.Atoi(strings.ReplaceAll(strings.Split(lines[1], ":")[1], " ", ""))
+	winOptions := 0
+	for t := 1; t < time; t++ {
+		distance := t * (time - t)
+		if distance > recordDistance {
+			winOptions++
+		}
+	}
+	fmt.Printf("Part2: %d\n", winOptions)
 }
